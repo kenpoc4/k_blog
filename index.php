0f096af54ccbaaa891da6b7ef330548062e69c06
Kenny Poncio <kenpoc4@gmail.com>
2020-04-29 23:37:34 -0600

    Paginación dinámica

--- index.php
+++ index.php
@@ -40,15 +40,7 @@
 
                     <!-- Paginación -->
                         <div class="card-body">
-                            <nav aria-label="Page navigation example">
-                                <ul class="pagination">
-                                    <li class="page-item"><a class="page-link" href="#">Anterior</a></li>
-                                    <li class="page-item active"><a class="page-link" href="#">1</a></li>
-                                    <li class="page-item"><a class="page-link" href="#">2</a></li>
-                                    <li class="page-item"><a class="page-link" href="#">3</a></li>
-                                    <li class="page-item"><a class="page-link" href="#">Siguiente</a></li>
-                                </ul>
-                                </nav>
+                            <?php get_template_part( 'template-parts/content', 'paginacion' )?>
                         </div>
                     <!-- Fin paginación -->
 
